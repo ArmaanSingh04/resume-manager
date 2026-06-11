6ba91439111ba33853e560602e1c7e74e2919bf0
Armaan <armaansinghlambhha@gmail.com>
2026-06-11 20:23:26 +0530

    created protected routes

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -1,9 +1,10 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth"
+import LogoutButton from "@/components/LogoutButton";
 
 export default async function Dashboard(){
     const session = await getServerSession(authOptions);
     return (
-        <div>{session?.user?.email}</div>
+        <div>{session?.user?.email} - <LogoutButton /></div>
     )
 }
---
+++ app/(protected)/layout.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+import { getServerSession } from "next-auth"
+import { authOptions } from "../api/auth/[...nextauth]/route"
+import { redirect } from "next/navigation"
+
+export default async function ProtectedLayout({children} : {children: React.ReactNode}){
+    const session = await getServerSession(authOptions)
+    
+    if(session?.user?.email){
+        return(
+            <div>{children}</div>
+        )
+    }
+    else{
+        redirect('/api/auth/signin')
+    }
+}
